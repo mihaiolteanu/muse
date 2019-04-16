7afe0a2ea5d6a14130e29c05e13b523461d61eb5
mihaiolteanu <mihai_olteanu@fastmail.fm>
2019-04-16 10:57:14 +0300

    One line per exported name in package.lisp

--- src/package.lisp
+++ src/package.lisp
@@ -1,9 +1,21 @@
 (defpackage :objects
   (:use :cl)
-  (:export artist artist-name artist-genres artist-similar artist-albums
-           genre genre-name
-           song song-name song-duration song-url song-lyrics
-           album album-name album-year album-songs))
+  (:export artist
+           artist-name
+           artist-genres
+           artist-similar
+           artist-albums
+           genre
+           genre-name
+           song
+           song-name
+           song-duration
+           song-url
+           song-lyrics
+           album
+           album-name
+           album-year
+           album-songs))
 
 (defpackage :parser
   (:use :cl :dexador :plump :lquery :objects)
@@ -13,21 +25,38 @@
   (:import-from :cl-utilities :split-sequence)
   (:shadowing-import-from :dexador :get)
   (:shadowing-import-from :dexador :delete)
-  (:export new-artist with-local-htmls
-           url-name clean-name))
+  (:export new-artist
+           with-local-htmls
+           parse-html
+           url-name
+           clean-name))
 
 (defpackage :persistence
   (:use :cl :sqlite :objects :parser)
-  (:export artists songs all-songs
-           albums genres all-genres all-genre-songs
-           similar insert-artist
+  (:export artists
+           songs
+           all-songs
+           albums
+           genres
+           all-genres
+           all-genre-songs
+           similar
+           insert-artist
            with-test-db))
 
 (defpackage :player
   (:use :cl :objects :uiop :bt)
-  (:export play-song play-songs continue-with-video
-           playing? next-song previous-song
-           play-pause kill-player seek quit))
+  (:export play-song
+           play-songs
+           continue-with-video
+           playing?
+           what-is-playing
+           next-song
+           previous-song
+           play-pause
+           kill-player
+           seek
+           quit))
 
 (defpackage :server
   (:use :cl :persistence :parser :objects :player :cl-who :hunchentoot))
